98a37facbbcc2a6cc8f4f4923782d60ee2bfc235
Promise267 <promrizal@gmail.com>
2024-06-14 11:13:01 +0545

    reigster-and-login-form-simple-UI-created

--- client/app/(auth)/login/page.tsx
+++ client/app/(auth)/login/page.tsx
@@ -1,11 +1,66 @@
-import React from 'react'
+'use client'
+import React, { ChangeEvent, useState }  from 'react'
+import Link from 'next/link'
 
-export default function page() {
+export default function Login() {
+
+  const [creds, setCreds] = useState({
+    username : "",
+    email : "",
+    password : ""
+  })
+
+  const handleCredentials = (e : ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const {name, value} = e.target
+    setCreds((prevValue) => {
+      return{
+        ...prevValue,
+        [name] : value
+      }
+    })
+  }
+
+
+
   return (
     <>
-    <div className=''>
-        <div>
-
+    <div className='mx-auto'>
+        <div className=' flex flex-col justify-center items-center h-screen space-y-3'>
+          <div className=' flex flex-row space-x-5'>
+            <input 
+              type="text"
+              className=' bg-gray-100 py-3 px-4' 
+              placeholder='Username'
+              name='username'
+              value={creds.username}
+              onChange={handleCredentials}
+            />
+            <input 
+              type="text"
+              className='bg-gray-100 py-3 px-4'
+              placeholder='Email'
+              name='email'
+              value={creds.email}
+              onChange={handleCredentials}
+            />
+          </div>
+          <div className='flex flex-col'>
+            <input 
+              type="password"
+              className='bg-gray-100 py-3 px-4'
+              placeholder='Password'
+              name='password'
+              value={creds.password}
+              onChange={handleCredentials}
+            />
+          </div>
+          <div>
+            <button className=' bg-blue-300 px-4 py-3 rounded-lg transition-all delay-75  hover:bg-green-500'>Login</button>
+          </div>
+          <div>
+            Already have an account? <Link href='/register'><span className=' text-blue-300 underline'>Register</span></Link>
+          </div>
         </div>
     </div>
     </>
--- client/app/(auth)/register/page.tsx
+++ client/app/(auth)/register/page.tsx
@@ -1,7 +1,57 @@
-import React from 'react'
+'use client'
+import React, { ChangeEvent } from 'react'
+import { useState } from 'react'
 
-export default function page() {
+export default function Register() {
+  const [creds, setCreds] = useState({
+    username : "",
+    email : "",
+    password : ""
+  })
+
+  const handleCredentials = (e : ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const {name, value} = e.target;
+    setCreds((prevValue) => {
+      return{
+        ...prevValue,
+        [name] : value
+      }
+    })
+
+
+  }
   return (
-    <div>page</div>
+    <>
+    <div className='mx-auto'>
+      <div className='flex flex-col justify-center items-center h-screen space-y-4'>
+          <div className='flex flex-row space-x-4'>
+            <input type="text" 
+            className=' bg-gray-100 px-3 py-4'
+            name='username'
+            value={creds.username}
+            onChange={handleCredentials}
+            />
+            <input type="email" 
+            className=' bg-gray-100 px-3 py-4'
+            name='email'
+            value={creds.email}
+            onChange={handleCredentials}
+            />
+          </div>
+          <div className='flex flex-col'>
+            <input type="password" 
+            className=' bg-gray-100 px-3 py-4'
+            name='password'
+            value={creds.password}
+            onChange={handleCredentials}
+            />
+          </div>
+          <div className=''>
+            <button className='bg-blue-300 px-3 py-4 rounded-lg'>Register</button>
+          </div>
+      </div>
+    </div>
+    </>
   )
 }
